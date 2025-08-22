45336b0447fae098b067980522ed2db1116cbeee
Yannkun67 <yannkun@bisphuket.ac.th>
2025-08-22 14:26:03 +0700

    m
    Co-Authored-By: gshaw-prog <226425488+gshaw-prog@users.noreply.github.com>

--- name = input("What is the students name?.py
+++ name = input("What is the students name?.py
@@ -1,1 +1,18 @@
-name = input("What is the students name?")
+fname = ""
+mname = ""
+lname = ""
+def mnamerepeat():
+    loop1 = 0
+    while loop1 == 0:
+        mname = input("What is the students middle name?")
+        yorn = input("Does the student have another middle name?")
+        if yorn == "No" or "no":
+            loop1 = 1
+        elif yorn == "Yes" or "yes":
+            return
+
+fname = input("What is the students first name?")
+mnamerepeat()
+lname = input("What is the students last name?")
+fname3char = fname[0:3]#used outside help
+print(fname3char)
