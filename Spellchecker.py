c4a54089812a440f3c84c9d1b81d33d14b27f832
Yannkun67 <yannkun@bisphuket.ac.th>
2025-09-01 10:48:43 +0700

    Update Spellchecker.py

--- Spellchecker.py
+++ Spellchecker.py
@@ -3,11 +3,8 @@ text = open("mystery-text.txt", "r")
 dict = open("dictionary.txt", "r")
 dline = dict.readline()
 loop = 0
-# while dline != "":
-    # print(dline.strip())
-    # dline = dict.readline()
 word = text.readline()
 word = word.strip()
 word = word.lower()
-word =word.translate(str.maketrans('', '', string.punctuation))
-print(word)
+word = word.translate(str.maketrans('', '', string.punctuation))
+
