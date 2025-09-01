5bfd025822d38bf07077037f6b38ff443bd81634
Yannkun67 <yannkun@bisphuket.ac.th>
2025-09-01 14:43:46 +0700

    i

--- Spellchecker.py
+++ Spellchecker.py
@@ -1,10 +1,17 @@
-import string
-text = open("mystery-text.txt", "r")
-dict = open("dictionary.txt", "r")
-dline = dict.readline()
-loop = 0
-word = text.readline()
-word = word.strip()
-word = word.lower()
-word = word.translate(str.maketrans('', '', string.punctuation))
-
+def Spellcheck():
+    import string
+    text = open("mystery-text.txt", "r")
+    dicti = open("dictionary.txt", "r")
+    dline = dicti.readline()
+    words = []
+    lines = 0
+    wcount = 0
+    for line in text: #loops through the file an puts it ito a list
+        line = line.strip()
+        line = line.lower()
+        line = line.translate(str.maketrans('', '', string.punctuation)) #got chatgpt to help with this
+        for word in line.split():
+            words.append(word)
+            wcount = wcount + 1
+    print (wcount)
+Spellcheck()
--- TempertaruteProject.py
+++ TempertaruteProject.py
@@ -1,33 +1,35 @@
-loop = 0
-maxtemp = 0
-lowtemp = 100
-tempincrease = 0
-tot = 0
-prevtemp = 100
-#all the variables for the program
-temp = open("hk-temperatures-2024.txt", "r")
-line = temp.readline()
-line = line.strip()
-while line != "":
+def temperature_tracker():
+    loop = 0
+    maxtemp = 0
+    lowtemp = 100
+    tempincrease = 0
+    tot = 0
+    prevtemp = 100
+    #all the variables for the program
+    temp = open("hk-temperatures-2024.txt", "r")
     line = temp.readline()
-    line = line.strip() #reads the line from the file and removes extra stuff
-    if line:
-        line = float(line)
-        if line > maxtemp: #checks if the temps are higher or lower than the current max/low temp
-            maxtemp = line
-        elif line < lowtemp:
-            lowtemp = line
-        tot = tot + line #adds the temperature to the total for the av
-        if line > prevtemp:#checks if the temperature increased from the day befro
-            tempincrease = tempincrease + 1
-            prevtemp = line
-        else:
-            prevtemp = line
+    line = line.strip()
+    while line != "":
+        line = temp.readline()
+        line = line.strip() #reads the line from the file and removes extra stuff
+        if line:
+            line = float(line)
+            if line > maxtemp: #checks if the temps are higher or lower than the current max/low temp
+                maxtemp = line
+            elif line < lowtemp:
+                lowtemp = line
+            tot = tot + line #adds the temperature to the total for the av
+            if line > prevtemp:#checks if the temperature increased from the day befro
+                tempincrease = tempincrease + 1
+                prevtemp = line
+            else:
+                prevtemp = line
 
-        loop = loop + 1
-avg = tot / loop #calculates the average temperature
+            loop = loop + 1
+    avg = tot / loop #calculates the average temperature
+    print("The average temperature is", avg)
+    print("The highest temperature is", maxtemp)
+    print("The lowest temperature is", lowtemp)
+    print("The number of days the temperature increased is", tempincrease)
 
-print("The average temperature is", avg)
-print("The highest temperature is", maxtemp)
-print("The lowest temperature is", lowtemp)
-print("The number of days the temperature increased is", tempincrease)
+temperature_tracker()
