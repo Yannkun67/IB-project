20426cdcc1eeb9ea9b5f66435e39ad65b6d0dca9
Yannkun67 <yannkun@bisphuket.ac.th>
2025-09-01 10:57:22 +0700

    hg

--- TempertaruteProject.py
+++ TempertaruteProject.py
@@ -4,29 +4,28 @@ lowtemp = 100
 tempincrease = 0
 tot = 0
 prevtemp = 100
-temp = float(0)
+#all the variables for the program
 temp = open("hk-temperatures-2024.txt", "r")
 line = temp.readline()
 line = line.strip()
-line = temp.readline()
 while line != "":
     line = temp.readline()
-    line = line.strip()
+    line = line.strip() #reads the line from the file and removes extra stuff
     if line:
         line = float(line)
-        if line > maxtemp:
+        if line > maxtemp: #checks if the temps are higher or lower than the current max/low temp
             maxtemp = line
         elif line < lowtemp:
             lowtemp = line
-        tot = tot + line
-        if line > prevtemp:
+        tot = tot + line #adds the temperature to the total for the av
+        if line > prevtemp:#checks if the temperature increased from the day befro
             tempincrease = tempincrease + 1
             prevtemp = line
         else:
             prevtemp = line
 
         loop = loop + 1
-avg = tot / loop
+avg = tot / loop #calculates the average temperature
 
 print("The average temperature is", avg)
 print("The highest temperature is", maxtemp)
